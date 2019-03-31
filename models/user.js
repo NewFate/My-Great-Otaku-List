de558f16f9b9224d907ada43298cad7b87cfe0d8
shovon77 <shovonmahmud@Shovons-MacBook-Pro-4.local>
2019-03-31 14:48:30 -0400

    Added password checking functionality and finding by email

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 
 const mongoose = require('mongoose');
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const ReviewsSchema = new mongoose.Schema({
 	animeName: String,
@@ -10,16 +12,72 @@ const ReviewsSchema = new mongoose.Schema({
 
 // User model, reviews will be embedded in the user model
 const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
+    name: {
+    	type: String,
+    	required: true,
+    	minlength: 1,
+    	trim: true
+    },
+    email: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true, // trim whitespace
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message: 'Not valid email'
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6
+	},
     dateOfBirth: String,
-    imageURL: String,
-    averageScore: Number,
+    //imageURL: String,
     reviews: [ReviewsSchema]
 });
 
 //AnimeSchema.index({name: "text", averageScore: "Number"});
 //AnimeSchema.index({name: "text"});
+// Our own student finding function 
+UserSchema.statics.findByEmailPassword = function(email, password) {
+	const User = this
+
+	return User.findOne({email: email}).then((user) => {
+		if (!user) {
+			return Promise.reject()
+		}
+
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (error, result) => {
+				if (result) {
+					resolve(user);
+				} else {
+					reject();
+				}
+			})
+		})
+	})
+}
+
+// This function runs before saving user to database
+UserSchema.pre('save', function(next) {
+	const user = this
+
+	if (user.isModified('password')) {
+		bcrypt.genSalt(10, (error, salt) => {
+			bcrypt.hash(user.password, salt, (error, hash) => {
+				user.password = hash
+				next()
+			})
+		})
+	} else {
+		next();
+	}
+
+})
 
 
 const User = mongoose.model('User', UserSchema);
