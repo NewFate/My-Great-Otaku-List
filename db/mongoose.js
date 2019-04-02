82357022064e0a9355a58187a301ad6e31f3d43a
Teo Magnino Chaban <teochaban@mail.utoronto.ca>
2019-04-02 13:44:41 -0400

    Still trying to fix atlas +  heroku iteraction

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,16 +2,9 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/GreatAnimeListAPI', { useNewUrlParser: true, useCreateIndex: true}, function(err, db){
-	if(err){
-		log("DEU RUIM");
-		log(err);
-	}else log("DEU BOM");
-}).catch((error) => {
-	log("ERROU");
-	log(error);
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/GreatAnimeListAPI', { useNewUrlParser: true, useCreateIndex: true}).catch(function (reason) {
+    console.log('Unable to connect to the mongodb instance. Error: ', reason);
 });
-
 module.exports = {
 	mongoose
 }
