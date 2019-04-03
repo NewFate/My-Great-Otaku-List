9103e77885da16d67c1b5ba4e8318c877c77b06c
Ronald Ong <ronald.ong@mail.utoronto.ca>
2019-04-03 12:20:52 -0400

    added server call to all anime page

--- public/js/AllAnime.js
+++ public/js/AllAnime.js
@@ -13,71 +13,56 @@ let all_anime_list = {
 	"Anime5": ["mob.gif", "Mob Psycho 100 II", "A cool anime", "10/10"]
 };
 
-//Phase 2: get the anime list from the server
-const url = '/anime';
-// Create our request constructor with all the parameters we need
-const request = new Request(url, {
-        method: 'get', 
-        headers: {
-            'Accept': 'application/json, text/plain, */*', //GETTING SOME STRANGE HTTPS ERROR
-            'Content-Type': 'application/json'
-        },
-    });
-    fetch(request)
-    .then(function(res) {
-        // Handle response we get from the API
-        // Usually check the error codes to see what happened
-        if (res.status === 200) {
-            console.log('Got the anime list')
-            const animeList = JSON.parse(res.json())
-           
-        } else {
-            
-     		 console.log('Did not get anime list')
-        }
-        console.log(res)
-        
-    }).catch((error) => {
-        console.log(error)
-    })
 
 //updates the top ten anime list
 function update_all_anime_list() {
 
-	const animeTable = all_anime_table.getElementsByTagName("table")[0];
-	//we would need to get the anime list from the server
-	//a dummy object is used for now
-	for (let key in all_anime_list)
-	{
-		const tr = document.createElement('tr');
+	const url = '/animeinfo';
 
-		const tdImg = document.createElement('td');
-		const tdTitle = document.createElement('td');
-		const tdDesc = document.createElement('td');
-		const tdScore = document.createElement('td');
+	fetch(url).then((res) => {
+		if(res.status == 200){
+			return res.json();
+		}else {
+			alert('Could not get Anime');
+		}
+	}).then((json) => {
+		
 
-		const animeImg = document.createElement('img');
+		const animeTable = all_anime_table.getElementsByTagName("table")[0];
+		//we would need to get the anime list from the server
+		//a dummy object is used for now
+		for (let key in json)
+		{
+			const tr = document.createElement('tr');
 
-		//get dummy data (we will be using the data structure fetched from the server here)
-		tdTitle.textContent = all_anime_list[key][1];
-		tdDesc.textContent = all_anime_list[key][2];
-		tdScore.textContent = all_anime_list[key][3];
-	
-		animeImg.className = "anime_tile";
-		animeImg.src = all_anime_list[key][0];
+			const tdImg = document.createElement('td');
+			const tdTitle = document.createElement('td');
+			const tdDesc = document.createElement('td');
+			const tdScore = document.createElement('td');
 
-		//construct the element
-		tdImg.appendChild(animeImg);
+			const animeImg = document.createElement('img');
 
-		tr.appendChild(tdImg);
-		tr.appendChild(tdTitle);
-		tr.appendChild(tdDesc);
-		tr.appendChild(tdScore);
+			//get dummy data (we will be using the data structure fetched from the server here)
+			tdTitle.textContent = json[key].name;
+			tdDesc.textContent = json[key].description;
+			tdScore.textContent = json[key].averageScore;
+		
+			animeImg.className = "anime_tile";
+			animeImg.src = json[key].imageURL;
 
-		//Add the element to the all anime table
-		animeTable.appendChild(tr);
-			
-	}
+			//construct the element
+			tdImg.appendChild(animeImg);
+
+			tr.appendChild(tdImg);
+			tr.appendChild(tdTitle);
+			tr.appendChild(tdDesc);
+			tr.appendChild(tdScore);
+
+			//Add the element to the all anime table
+			animeTable.appendChild(tr);
+				
+		}
+	})
 	
 }
 
@@ -95,13 +80,14 @@ function go_to_anime_page(e) {
 
 	if(e.target.tagName.toLowerCase() === 'img')
 	{
-		// we would need the title from the server data
-		const animeTitle = e.target.parentNode.nextSibling.textContent;
 		
 	    //for now just go to a generic anime page, would need the data from the server 
-		window.location.href = "/anime";
+		let animeTitle = e.target.parentNode.nextSibling.textContent;
+		animeTitle = animeTitle.replace(/\s+/g, "_");
+		
+		window.location.href = "/anime/" + animeTitle ;
 		//here we would send the anime title to the next page to load it
-		load(animeTitle);
+		
 	}
 
 }
